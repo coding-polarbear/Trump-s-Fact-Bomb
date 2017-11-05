263ba775c5c344ca35fc9d51617091efb50eb32d
baehyeonbin <baehynebin@hotmail.co.kr>
2017-11-05 09:20:44 +0900

    아이콘 적용

--- app/src/main/java/com/example/baehyeonbin/highthon/MainActivity.java
+++ app/src/main/java/com/example/baehyeonbin/highthon/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     private void init() {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        toolbar.setTitle(getString(R.string.app_name));
         viewPager = (ViewPager) findViewById(R.id.ViewPager);
         tabLayout = (TabLayout) findViewById(R.id.TabLayout);
     }
--- app/src/main/java/com/example/baehyeonbin/highthon/services/CommentService.java
+++ app/src/main/java/com/example/baehyeonbin/highthon/services/CommentService.java
@@ -1,9 +1,21 @@
 package com.example.baehyeonbin.highthon.services;
 
+import com.example.baehyeonbin.highthon.beans.CommentListGet;
+import com.example.baehyeonbin.highthon.beans.Success;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
 /**
  * Created by baehyeonbin on 2017. 11. 5..
  */
 
 public interface CommentService {
+    @POST("/comments/{idx}")
+    Call<Success> createComments(@Path("idx") int postIdx);
 
+    @GET("/comments/{idx}")
+    Call<CommentListGet> getComments(@Path("idx") int postIdx);
 }
